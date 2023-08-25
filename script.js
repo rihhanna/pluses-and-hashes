e129e5f695b5b8772505c0ea37f31154f08a1ea1
rihhana <hrihhana@gmail.com>
2023-08-25 18:05:30 +0300

    done it

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Good Luck 💪🏾
+function plusHashCount(str) {
+    // initialize a counter variable
+    let count = 0;
+    // loop through the string
+    for (let i = 0; i < str.length; i++) {
+      // check if the current character is a hash or a plus
+      if (str[i] === "#" || str[i] === "+") {
+        // increment the counter
+        count++;
+      }
+    }
+    // return the counter
+    return count;
+  }
+console.log(plusHashCount("###+"))  
+console.log(plusHashCount("##+++#"))
+console.log(plusHashCount("#+++#+#++#"))
+console.log(plusHashCount(""))
